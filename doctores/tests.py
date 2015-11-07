4315edc1d832e1a428dc7e913ad4d1cef5ae17c6
JBaez17 <kirito_baz@outlook.com>
2015-11-06 20:12:41 -0600

    La vista basica ahora retorna un HTML basico

--- doctores/tests.py
+++ doctores/tests.py
@@ -1,5 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
 from doctores.views import home_page
 
 
@@ -8,3 +10,10 @@ class TestPaginaPrincipal(TestCase):
     def test_url_principal_retorna_a_vista_pagina_principal(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_pagina_principal_retorna_el_html_correcto(self):
+        request = HttpRequest()
+        response = home_page(request)
+        self.assertTrue(response.content.startswith("<html>"))
+        self.assertIn(b'<title>Control Hospital</title>', response.content)
+        self.assertTrue(response.content.endswith("</html>"))
