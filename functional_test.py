1b2248fe06ffcdeb1d4190a238b25cfedcee1614
JBaez17 <kirito_baz@outlook.com>
2015-11-06 18:14:21 -0600

    Modificado el archivo functional_test.py, se ha implementado la estructura para el uso de unittest, se creo el metodo para la prueba de la pagina principal, setUP, y tearDown

--- functional_test.py
+++ functional_test.py
@@ -1,7 +1,23 @@
 from selenium import webdriver
+import unittest
 
+class TestNuevoVisitante(unittest.TestCase):
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-assert 'Django' in browser.title
+    def test_ver_pagina_principal(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('Control Hospital', self.browser.title)
+        self.fail('Completa el test!!!!')
+
+    def tearDown(self):
+        self.browser.quit()
+
+if __name__ == '__main__':
+    unittest.main()
