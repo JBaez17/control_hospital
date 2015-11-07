a47b0c935a79b904f8db56456c197df26e926cf4
JBaez17 <kirito_baz@outlook.com>
2015-11-06 21:21:16 -0600

    Se refactorizo la prueba para revisar el html de la pagina principal

--- doctores/tests.py
+++ doctores/tests.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from doctores.views import home_page
 
@@ -14,6 +15,10 @@ class TestPaginaPrincipal(TestCase):
     def test_pagina_principal_retorna_el_html_correcto(self):
         request = HttpRequest()
         response = home_page(request)
-        self.assertTrue(response.content.startswith("<html>"))
+        """self.assertTrue(response.content.startswith("<html>"))
         self.assertIn(b'<title>Control Hospital</title>', response.content)
-        self.assertTrue(response.content.endswith("</html>"))
+        self.assertTrue(response.content.endswith("</html>"))"""
+        expected_html = render_to_string('home.html')
+        # se usa decode() para convertir la response.content
+        # de bytes a una cadena unicode
+        self.assertEqual(response.content.decode(), expected_html)
